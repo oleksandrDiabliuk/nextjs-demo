66b09dff5222961255691087f85203daaaaa59bd
odiabliuk <o.diabliuk@gmail.com>
2023-05-31 17:06:51 +0300

    changed fallback value

--- pages/[meetupID]/index.js
+++ pages/[meetupID]/index.js
@@ -31,7 +31,7 @@ export async function getStaticPaths() {
 	client.close();
 
 	return {
-		fallback: true,
+		fallback: "blocking",
 		paths: meetups.map((meetup) => ({
 			params: {
 				meetupID: meetup._id.toString(),
